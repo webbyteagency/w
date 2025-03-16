077dd015c404a83e10e28684c699c4142d4fa175
webbyteagency <132273113+webbyteagency@users.noreply.github.com>
2025-03-16 11:34:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,8 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/main.css',
-        '/assets/img/logo.png'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/serviceWorker.js')
+            .then((registration) => {
+                console.log('Service worker registered.', registration);
+            });
+    });
+}
